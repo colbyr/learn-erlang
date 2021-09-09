1920200da589d5e817d7ade82919601bb2df72f3
Colby Rabideau <478109+colbyr@users.noreply.github.com>
2021-09-09 10:05:38 -0400

    basic schema

--- tlv8/tlv8.erl
+++ tlv8/tlv8.erl
@@ -1,6 +1,7 @@
 -module(tlv8).
 -export([
-  decode/1
+  decode/1,
+  decode_by_schema/2
 ]).
 
 decode(Binary) ->
@@ -16,4 +17,32 @@ decode(<<Type:8, Size:8, Value:(Size)/binary, NextBinary/binary>>, Acc) ->
   decode(NextBinary, NextAcc);
 decode(Binary, Acc) ->
   maps:put(invalid, Binary, Acc).
+
+% #{ 16#01 => {name, value_converter} }
+%
+
+get_key_by_schema(Key, Schema) ->
+  case maps:get(Key, Schema, undefined) of
+    {NamedKey, _} -> NamedKey;
+    undefined -> Key
+  end.
+
+get_value_by_schema(Key, Value, Schema) ->
+  case maps:get(Key, Schema, undefined) of
+    {_, bytes} -> Value;
+    {_, integer} ->
+      <<Int/integer>> = Value,
+      Int;
+    {_, utf8} -> binary_to_list(Value);
+    {_, _} -> Value;
+    undefined -> Value
+  end.
+
+decode_by_schema(Schema, Binary) ->
+  RawData = decode(Binary),
+  SchemafiedList = [{
+    get_key_by_schema(K, Schema),
+    get_value_by_schema(K, V, Schema)
+   } || {K, V} <- maps:to_list(RawData)],
+  maps:from_list(SchemafiedList).
 
--- tlv8/tlv8_tests.erl
+++ tlv8/tlv8_tests.erl
@@ -32,3 +32,39 @@ decode_test() ->
     tlv8:decode(<< 16#ff, 16#05, 16#01, 16#02, 16#03 >>)
   ).
 
+decode_by_schema_test() ->
+  ?assertMatch(
+    #{ 16#ff := <<16#01, 16#02, 16#03>> },
+    tlv8:decode_by_schema(
+      #{},
+      <<16#ff, 16#03, 16#01, 16#02, 16#03>>
+    )
+  ),
+
+  ?assertMatch(
+    #{ test := 1 },
+    tlv8:decode_by_schema(
+      #{ 16#ff => {test, integer} },
+      <<16#ff, 16#01, 16#01>>
+    )
+  ),
+
+  ?assertMatch(
+    #{ test := "wow" },
+    tlv8:decode_by_schema(
+      #{ 16#ff => {test, utf8} },
+      <<16#ff, 16#03, 119, 111, 119>>
+    )
+  ),
+
+  ?assertMatch(
+    #{ one := 1, two := "two", three := <<3>> },
+    tlv8:decode_by_schema(
+      #{ 16#01 => {one, integer}, 16#02 => {two, utf8}, 16#03 => {three, bytes} },
+      <<
+        16#01, 16#01, 16#01,
+        16#02, 16#03, 116, 119, 111,
+        16#03, 16#01, 16#03
+      >>
+    )
+  ).

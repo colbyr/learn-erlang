99e92682d0aaa311a256ed93198729154b42ad42
Colby Rabideau <478109+colbyr@users.noreply.github.com>
2021-09-09 09:00:50 -0400

    throw on invalid input

--- tlv8/tlv8.erl
+++ tlv8/tlv8.erl
@@ -4,11 +4,16 @@
 ]).
 
 decode(Binary) ->
-  decode(Binary, #{}).
+  case decode(Binary, #{}) of
+    #{ invalid := Invalid } -> erlang:error(badarg, Invalid);
+    Result -> Result
+  end.
 
 decode(<<>>, Acc) ->
   Acc;
 decode(<<Type:8, Size:8, Value:(Size)/binary, NextBinary/binary>>, Acc) ->
   NextAcc = maps:put(Type, Value, Acc),
-  decode(NextBinary, NextAcc).
+  decode(NextBinary, NextAcc);
+decode(Binary, Acc) ->
+  maps:put(invalid, Binary, Acc).
 
